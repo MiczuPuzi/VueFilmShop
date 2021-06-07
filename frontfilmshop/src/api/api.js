4e9f3c9fbddf2aaa79e4da918e6b2963beae606f
Konefal Jakub <konefaljakub@gmail.com>
2021-06-07 19:30:10 +0200

    Connect register form with backend

--- frontfilmshop/src/api/api.js
+++ frontfilmshop/src/api/api.js
@@ -1,54 +1,43 @@
-import axios from "axios";
+import axios from 'axios'
 
 let backendAddress = 'http://localhost:3000/api/'
 
 function sendRequest(address, endpoint, method, body, headers) {
-    headers['Content-Type'] = 'application/json'
-    return axios({
-        method: method.toLowerCase(),
-        url: address + endpoint,
-        headers: headers,
-        data: body
-    })
+  headers['Content-Type'] = 'application/json'
+  return axios({
+    method: method.toLowerCase(),
+    url: address + endpoint,
+    headers: headers,
+    data: body,
+  })
 }
 
 export function getAllFilms() {
-    return sendRequest(
-        backendAddress,
-        'films',
-        'get',
-        {},
-        {}
-    )
-
+  return sendRequest(backendAddress, 'films', 'get', {}, {})
 }
 
 export function getAllOpinions() {
-    return sendRequest(
-        backendAddress,
-        'opinions',
-        'get',
-        {},
-        {}
-    )
+  return sendRequest(backendAddress, 'opinions', 'get', {}, {})
 }
 
 export function createOpinion(opinion) {
-    return sendRequest(
-        backendAddress,
-        'opinions',
-        'post',
-        opinion,
-        {}
-    )
+  return sendRequest(backendAddress, 'opinions', 'post', opinion, {})
 }
 
-export function deleteOpinion(opinionId){
-    return sendRequest(
-        backendAddress,
-        'opinions/' + opinionId,
-        'delete',
-        {},
-        {}
-    )
+export function deleteOpinion(opinionId) {
+  return sendRequest(backendAddress, 'opinions/' + opinionId, 'delete', {}, {})
+}
+
+export function registerUser(email, password) {
+  return sendRequest(
+    backendAddress,
+    'register',
+    'post',
+    { email, password },
+    {}
+  )
+}
+
+export function loginUser(email, password) {
+  return sendRequest(backendAddress, '/login', 'post', { email, password }, {})
 }
--- frontfilmshop/src/views/Login.vue
+++ frontfilmshop/src/views/Login.vue
@@ -1,74 +1,136 @@
 <template>
-      <v-dialog v-model="dialog" persistent max-width="600px" min-width="360px">
-        <div>
-          <v-tabs v-model="tab" show-arrows background-color="deep-purple accent-4" icons-and-text dark grow>
-            <v-tabs-slider color="purple darken-4"></v-tabs-slider>
-            <v-tab v-for="i in tabs" :key="i">
-              <v-icon large>{{ i.icon }}</v-icon>
-              <div class="caption py-1">{{ i.name }}</div>
-            </v-tab>
-            <v-tab-item>
-              <v-card class="px-4">
-                <v-card-text>
-                  <v-form ref="loginForm" v-model="valid" lazy-validation>
-                    <v-row>
-                      <v-col cols="12">
-                        <v-text-field v-model="loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field v-model="loginPassword" :append-icon="show1?'mdi-eye':'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                      </v-col>
-                      <v-col class="d-flex" cols="12" sm="6" xsm="12">
-                      </v-col>
-                      <v-spacer></v-spacer>
-                      <v-col>
-                        <v-btn x-large color="success" to="/home">Back</v-btn>
-                      </v-col>
-                      <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
-                        <v-btn x-large block :disabled="!valid" color="success" @click="validate"> Login </v-btn>
-                      </v-col>
-                    </v-row>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </v-tab-item>
-            <v-tab-item>
-              <v-card class="px-4">
-                <v-card-text>
-                  <v-form ref="registerForm" v-model="valid" lazy-validation>
-                    <v-row>
-                      <v-col cols="12">
-                        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
-                      </v-col>
-                      <v-spacer></v-spacer>
-                      <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
-                        <v-btn x-large block :disabled="!valid" color="success" @click="validate">Register</v-btn>
-                      </v-col>
-                    </v-row>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </v-tab-item>
-          </v-tabs>
-        </div>
-      </v-dialog>
+  <v-dialog v-model="dialog" persistent max-width="600px" min-width="360px">
+    <div>
+      <v-tabs
+        v-model="tab"
+        show-arrows
+        background-color="deep-purple accent-4"
+        icons-and-text
+        dark
+        grow
+      >
+        <v-tabs-slider color="purple darken-4"></v-tabs-slider>
+        <v-tab v-for="i in tabs" :key="i">
+          <v-icon large>{{ i.icon }}</v-icon>
+          <div class="caption py-1">{{ i.name }}</div>
+        </v-tab>
+        <v-tab-item>
+          <v-card class="px-4">
+            <v-card-text>
+              <v-form ref="loginForm" v-model="valid" lazy-validation>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="loginEmail"
+                      :rules="loginEmailRules"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="loginPassword"
+                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :rules="[rules.required, rules.min]"
+                      :type="show1 ? 'text' : 'password'"
+                      name="input-10-1"
+                      label="Password"
+                      hint="At least 8 characters"
+                      counter
+                      @click:append="show1 = !show1"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col class="d-flex" cols="12" sm="6" xsm="12"> </v-col>
+                  <v-spacer></v-spacer>
+                  <v-col>
+                    <v-btn x-large color="success" to="/home">Back</v-btn>
+                  </v-col>
+                  <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                    <v-btn
+                      x-large
+                      block
+                      :disabled="!valid"
+                      color="success"
+                      @click="validate"
+                    >
+                      Login
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card class="px-4">
+            <v-card-text>
+              <v-form ref="registerForm" v-model="valid" lazy-validation>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="email"
+                      :rules="emailRules"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="password"
+                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :rules="[rules.required, rules.min]"
+                      :type="show1 ? 'text' : 'password'"
+                      name="input-10-1"
+                      label="Password"
+                      hint="At least 5 characters"
+                      counter
+                      @click:append="show1 = !show1"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      block
+                      v-model="verify"
+                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :rules="[rules.required, passwordMatch]"
+                      :type="show1 ? 'text' : 'password'"
+                      name="input-10-1"
+                      label="Confirm Password"
+                      counter
+                      @click:append="show1 = !show1"
+                    ></v-text-field>
+                  </v-col>
+                  <v-spacer></v-spacer>
+                  <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+                    <v-btn
+                      x-large
+                      block
+                      :disabled="!valid"
+                      color="success"
+                      @click="registerNewUser(email, password)"
+                      >Register</v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </div>
+  </v-dialog>
 </template>
 
-
 <script>
+import { registerUser } from '../api/api'
+
 export default {
-  name: "Login",
+  name: 'Login',
 
   computed: {
     passwordMatch() {
-      return () => this.password === this.verify || "Password must match";
-    }
+      return () => this.password === this.verify || 'Password must match'
+    },
   },
 
   methods: {
@@ -78,46 +140,44 @@ export default {
       }
     },
     reset() {
-      this.$refs.form.reset();
+      this.$refs.form.reset()
     },
     resetValidation() {
-      this.$refs.form.resetValidation();
-    }
+      this.$refs.form.resetValidation()
+    },
+    registerNewUser() {
+      registerUser(this.email, this.password)
+    },
   },
-  data:() => ({
+  data: () => ({
     dialog: true,
     tab: 0,
     tabs: [
-      {name:"Login", icon:"mdi-account"},
-      {name:"Register", icon:"mdi-account-outline"}
+      { name: 'Login', icon: 'mdi-account' },
+      { name: 'Register', icon: 'mdi-account-outline' },
     ],
     valid: true,
-
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    verify: "",
-    loginPassword: "",
-    loginEmail: "",
+    email: '',
+    password: '',
+    verify: '',
+    loginPassword: '',
+    loginEmail: '',
     loginEmailRules: [
-      v => !!v || "Required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      (v) => !!v || 'Required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
     ],
     emailRules: [
-      v => !!v || "Required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      (v) => !!v || 'Required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
     ],
 
     show1: false,
     rules: {
-      required: value => !!value || "Required.",
-      min: v => (v && v.length >= 8) || "Min 8 characters"
-    }
-})
+      required: (value) => !!value || 'Required.',
+      min: (v) => (v && v.length >= 5) || 'Min 5 characters',
+    },
+  }),
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
